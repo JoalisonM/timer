bab72b5651dd2fcc30f936de74b69a691dd77db8
JoalisonM <joalisonmatheus10@gmail.com>
2022-09-23 20:23:10 -0300

    created project timer

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Timer, Scroll } from 'phosphor-react'
+
+import { HeaderContainer } from './styles'
+
+import logoIgnite from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <a href="">
+          <Timer size={24} />
+        </a>
+        <a href="">
+          <Scroll size={24} />
+        </a>
+      </nav>
+    </HeaderContainer>
+  )
+}
